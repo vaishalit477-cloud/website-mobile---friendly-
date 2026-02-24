e88e4293aba5d76f7222dd7eac1a05713a8af35f
vaishalit477-cloud <vaishalit432@gmail.com>
2026-02-24 11:59:41 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const nav = document.querySelector(".nav-links");
+    nav.classList.toggle("active");
+}function toggleMenu() {
+    const nav = document.querySelector(".nav-links");
+    nav.classList.toggle("active");
+}
